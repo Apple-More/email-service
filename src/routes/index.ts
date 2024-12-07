d88ba5ea332cecac6a80b63a87e5fc035acb938b
Nipun-Milinda <nipunmilinda@gmail.com>
2024-12-07 11:38:33 +0530

    configure allowRoles middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { sendEmails } from '../controllers/email-controller';
+import allowRoles from '../middlewares/allow-roles';
 
 const router = Router();
 
-router.post('/send', sendEmails);
+router.post('/send', allowRoles('Customer', 'Admin', 'SuperAdmin'), sendEmails);
 
 export default router;
